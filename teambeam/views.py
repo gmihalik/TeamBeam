091ce88df82b5cffa9b76cb306e5e104a6b575cb
Gregory Mihalik <gregory.mihalik@gmail.com>
2015-04-24 18:52:40 +0000

    Inital Commit of TeamBeam

---
+++ teambeam/views.py
@@ -0,0 +1,12 @@
+#Function or class based views
+#Views are controllers
+#Templates are views
+
+from django.shortcuts import render
+
+
+#def home(request):
+#    context = {}
+#    template = "home.html"
+#    return render(request, template, context)
+    
